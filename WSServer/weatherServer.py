6638bb27ce7b19938762301d3be21435f0b2189a
SamHillcoat <ham.sillcoat@gmail.com>
2020-12-26 22:37:38 +1100

    start on post req handling

--- WSServer/weatherServer.py
+++ WSServer/weatherServer.py
@@ -1,12 +1,68 @@
-from flask import Flask, render_template
+"""
+weatherServer.py
+
+The main flask server file for the weather station server
+Serves the web page and handles incoing data from the station
+
+Written by: Sam Hillcoat
+Last Modified: 26/12/20 9:15 PM
+
+"""
+
+from flask import Flask, render_template, request, make_response
+import datetime, json, os
+
 app = Flask(__name__)
+
 # Main server-side code for weather station
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# Function for handling incoming data and saving it to the server
+cwdir = os.path.dirname(__file__) # Current working directory
+json_path = os.path.join(cwdir, 'static/stored_data.json')
+
+@app.route('/senddata', methods=['POST'])
+def send_data():
+    wind_speed = request.args.get('windspeed', default = None, type=float)
+    wind_dir = request.args.get('winddir', default = None, type=int)
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%a %b %d %Y %X")
+    print(request)
+
+    new_data = {'x':wind_speed, 'y':timestamp, 'dir':wind_dir}
+    #output_to_json(new_data)
+    print(new_data)
+    return make_response(
+        'Data Received',
+        200
+    )
+
+
+
+
+def output_to_json(new_data):
+    # new_data : data from POST request from send_data()
+    # Open the json file, read all json data into python object, add the new data,
+    # turn back into json data, overwrite the data into the file.
+
+    with open(json_path, "r") as json_file:
+        old_data = json.load(json_file) #type : list of dicts
+
+    old_data.append(new_data) # append the new data to the list
+
+    #output to file as json
+    with open(json_path, "w") as json_file:
+        json.dump(old_data, json_file) #type : list of dicts
+
+    
+
+
+
 
 if (__name__ == '__main__'):
     app.run(debug = True, host='0.0.0.0')
-#testtest3b
+
+
---
+++ fakeClient.py
@@ -0,0 +1,33 @@
+"""
+Testing file for weather server
+
+sends random data to the server via POST request
+acts as a fake weather station
+
+Written by: Sam Hillcoat
+Last Modified: 26/12/20 9:25 PM
+"""
+
+import requests
+from random import randint
+
+#URL = "https://deviotweather.com/senddata"
+#URL = "http://127.0.0.1:5000/senddata"
+URL = "https://deviotweather.free.beeceptor.com"
+
+def post_data(data):
+    response = requests.post(URL, data = data)
+    print(response)
+    
+
+def generate_wind_data():
+    windspeed = randint(0,30)
+    winddir = randint(0,359)
+    data = {"windspeed" : windspeed, "winddir": winddir}
+    post_data(data)
+    print(data)
+
+
+for i in range(0,1):
+    generate_wind_data()
+
